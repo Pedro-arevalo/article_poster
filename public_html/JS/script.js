ad895e0d3268fcee5dccb9fd939891e9c16b89c5
Pedro Arevalo <devpedroarevalo@gmail.com>
2022-02-19 04:12:48 -0400

    managing how to post a new article

--- public_html/JS/script.js
+++ public_html/JS/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const button_postArticle = document.getElementById('button_post');
+    button_postArticle.addEventListener('click', () => {
+        postNewArticle();
+    });
     getAllArticles();
 });
 
@@ -11,4 +15,18 @@ function getAllArticles() {
             showAllArticles(articles);
             setCounter(articles.length);
         });
+}
+
+function postNewArticle() {
+    let title = document.getElementById('inputPost_title').value;
+    let text = document.getElementById('inputPost_text').value;
+    let time = new Date();
+
+    let article = { title, text, time };
+
+    let options = {
+        method: 'POST',
+        'Content-Type': 'application/json',
+    };
+    fetch('http://localhost:5000/api/new', options);
 }
